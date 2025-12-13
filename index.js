848469da1217393faf77e47dc4e4dce7b3fb3e3e
Sarvam Patel <148036885+CoreTech7704@users.noreply.github.com>
2025-12-13 22:00:10 +0530

    fix: removed all vulnerabilities, updated security

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cookieParser = require("cookie-parser");
 const { connectToMongoDB } = require("./connect");
 const { checkForAuthentication, restrictTo } = require("./middlewares/auth");
 const URL = require("./models/url");
+const helmet = require("helmet");
 const rateLimit = require("express-rate-limit");
 
 const urlRoute = require("./routes/url");
@@ -11,7 +12,7 @@ const staticRoute = require("./routes/staticRouter");
 const userRoute = require("./routes/user");
 
 const app = express();
-const PORT = process.env.PORT || 5000;;
+const PORT = process.env.PORT || 5000;
 
 connectToMongoDB(
   process.env.MONGODB_URI ?? "mongodb://localhost:27017/short-url"
@@ -23,36 +24,39 @@ const createUrlLimiter = rateLimit({
   message: "Too many requests! Try again later.",
 });
 
+app.set("trust proxy", 1);
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
+app.use(helmet());
 app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: false, limit: "10kb" }));
 app.use(cookieParser());
 app.use(checkForAuthentication);
 app.use(express.static("public"));
 
-
-app.use("/url", restrictTo(["NORMAL", "ADMIN"]), urlRoute);
-app.use("/user", userRoute);
-app.use("/", staticRoute);
-app.use("/url", createUrlLimiter);
-
 app.get("/url/:shortId", async (req, res) => {
-  const shortId = req.params.shortId;
+  const { shortId } = req.params;
+
   const entry = await URL.findOneAndUpdate(
-    {
-      shortId,
-    },
-    {
-      $push: {
-        visitHistory: {
-          timestamp: Date.now(),
-        },
-      },
-    }
+    { shortId },
+    { $push: { visitHistory: { timestamp: Date.now() } } }
   );
+
+  if (!entry) {
+    return res.status(404).render("404");
+  }
+
   res.redirect(entry.redirectURL);
 });
+
+app.use(
+  "/url",
+  restrictTo(["NORMAL", "ADMIN"]),
+  createUrlLimiter,
+  urlRoute
+);
+app.use("/user", userRoute);
+app.use("/", staticRoute);
 
 app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));
